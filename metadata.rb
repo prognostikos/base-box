ac409060bec9edad87d01fc200ce098c026db916
Matt Rohrer <matt@prognostikos.com>
2013-08-20 15:27:25 +0200

    bump version to v1.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'matt@prognostikos.com'
 license          'Apache 2.0'
 description      'The basics for a server or workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 supports 'ubuntu'
 
