23e0f5564e39fc4a818c5a28e779a739bde4005e
Matt Rohrer <matt@prognostikos.com>
2014-01-02 11:53:45 +0100

    Default to no security settings as we're using this more with AWS

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default[:base_box][:include_security_settings]     = true
+default[:base_box][:include_security_settings]     = false
 default[:base_box][:include_troubleshooting_tools] = true
 default[:base_box][:include_development_tools]     = true
 default[:base_box][:include_mailer]                = true
